8bfaa05f104cbbbf134265e43ab8405007496462
AtharvKhanvilkar1410 <84334294+AtharvKhanvilkar1410@users.noreply.github.com>
2023-03-07 14:36:34 +0530

    final1.0

--- budgetplanner.py
+++ budgetplanner.py
@@ -32,55 +32,23 @@ def budgetplanner():
         </div>
         """, unsafe_allow_html=True)
     
-	
-    st.markdown("---")
-    st.title("Calculate The CAGR")
-    with st.expander(label="Know More About CAGR"):
-        st.write("CAGR (Compound Annual Growth Rate) measures your investments' average annual growth over a given period. When calculating CAGR, profits are assumed to be reinvested at the end of each year of the time horizon. In most cases, an investment cannot grow at the same rate year after year. Despite this, the CAGR calculator is widely used to compare alternative investments.")
-
-    rcol1,rcol2 = st.columns(2, gap='large')
-    with rcol1:
-        # with st.form(key="cagrcal", clear_on_submit=True):
-        start_value = st.number_input("Initial Value", format="%d", value=800000, step=1000)
-        end_value = st.number_input("Final Value Costs", format="%d", value=2100000, step=1000)
-        num_periods = st.number_input("Duration of Investment", format="%d", value=6)
-
-        CAGR = cagr(start_value, end_value, num_periods)
-        CAGR = CAGR*100
-        CAGR = round(CAGR, 2)
-        # st.metric(CAGR)
-        st.metric("CAGR",  f"{CAGR}")
-
-
-    df = pd.DataFrame({
-        'x' :['Initial Value','Final Value'],
-        'y' :[start_value,end_value]})
-    with rcol2:
-        fig1 = px.pie(
-            df,
-            values="y",names="x",
-            hole=0.55, 
-            
-            width=685
-        )
-
-        fig1.update_layout(
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)',
-
-        )
-        st.plotly_chart(fig1)
-    
-    st.markdown("---")
+   # st.title("Basic Tax Structure According to 2023")
+    # tax_data = pd.DataFrame(
+    #     data={"Investment":["5-Year Bank Fixed Deposit","Public Provident Fund (PPF)", "National Savings Certificate", "National Pension System (NPS)", "ELSS Funds","Unit Linked Insurance Plan (ULIP)","Sukanya Samriddhi Yojana (SSY)","Senior Citizen Saving Scheme (SCSS)"],
+    #     "Returns": ["6% to 7%", "7% to 8%", "7% to 8%", "12% to 14%", "	15% to 18%", "Varies with Plan Chosen","7.60%","7.40%"],
+    #     "Lock-in Period":["5 years", "15 years", "5 years", "Till Retirement", "3 years","5 years","N/A","5 years"],      
+    # })
+    # st.markdown("###")
+    # st.table(data=tax_data)
     st.markdown("###")
-    st.title("Basic Tax Structure According to 2023")
+    st.header("Basic Tax Structure According to 2023")
     tax_data = pd.DataFrame(
-        data={"Investment":["5-Year Bank Fixed Deposit","Public Provident Fund (PPF)", "National Savings Certificate", "National Pension System (NPS)", "ELSS Funds","Unit Linked Insurance Plan (ULIP)","Sukanya Samriddhi Yojana (SSY)","Senior Citizen Saving Scheme (SCSS)"],
-        "Returns": ["6% to 7%", "7% to 8%", "7% to 8%", "12% to 14%", "	15% to 18%", "Varies with Plan Chosen","7.60%","7.40%"],
-        "Lock-in Period":["5 years", "15 years", "5 years", "Till Retirement", "3 years","5 years","N/A","5 years"],      
+        data={"Income Range":["0 to Rs 2.5 lakh","Rs 2.5 lakh to Rs 5 lakh", "Rs 5 lakh to Rs 7.5 lakh", "Rs 7.50 lakh to Rs 10 lakh", "Rs 10 lakh to Rs 12.5 lakh","Rs 12.5 lakh to Rs 15 lakh","Above Rs 15 lakh"],
+        "Tax Applied": [" NIL", "5% above Rs 2.5 lakh", "Rs 12,500 + 10% above Rs 5 lakh", "Rs 37,500 + 15% above Rs 7.5 lakh", "Rs 75,000 + 20% above Rs 10 lakh", "Rs 125,000 + 25% above Rs 12.5 lakh","Rs 187,500 + 30% above Rs 15 lakh"],
     })
     st.markdown("###")
     st.table(data=tax_data)
+	
 
     st.markdown("---")    
     sum = 0
@@ -242,6 +210,97 @@ def budgetplanner():
     st.markdown("###")
     st.table(tax_deduction_data)
 	
+    st.markdown("---")  
+    st.title("Calculate The CAGR")
+    with st.expander(label="Know More About CAGR"):
+        st.write("CAGR (Compound Annual Growth Rate) measures your investments' average annual growth over a given period. When calculating CAGR, profits are assumed to be reinvested at the end of each year of the time horizon. In most cases, an investment cannot grow at the same rate year after year. Despite this, the CAGR calculator is widely used to compare alternative investments.")
+
+    rcol1,rcol2 = st.columns(2, gap='large')
+    with rcol1:
+        # with st.form(key="cagrcal", clear_on_submit=True):
+        start_value = st.number_input("Initial Value", format="%d", value=800000, step=1000)
+        end_value = st.number_input("Final Value Costs", format="%d", value=2100000, step=1000)
+        num_periods = st.number_input("Duration of Investment", format="%d", value=6)
+
+        CAGR = cagr(start_value, end_value, num_periods)
+        CAGR = CAGR*100
+        CAGR = round(CAGR, 2)
+        # st.metric(CAGR)
+        st.metric("CAGR",  f"{CAGR}")
+
+
+    df = pd.DataFrame({
+        'x' :['Initial Value','Final Value'],
+        'y' :[start_value,end_value]})
+    with rcol2:
+        fig1 = px.pie(
+            df,
+            values="y",names="x",
+            hole=0.55, 
+            
+            width=685
+        )
+
+        fig1.update_layout(
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0,0)',
+
+        )
+        st.plotly_chart(fig1)
+
+    st.markdown("---")  
+    st.title("Here are some invewstement suggestion to save your Taxes:")
+    st.markdown("###")
+    with st.expander(label="Disclaimer"):
+        st.write("Some of the following schemes fall under the Section 80C; according to which you can only get benefits upto 1.5 Lakh collectively.")
+
+    col1, col2, col3,  = st.columns(3, gap="large")
+    with col1:
+        card_container1 = st.container()
+        with card_container1:
+            st.header("PPF")
+            st.subheader("1.5 lakh")
+            st.write("The current PPF interest rate is 7.1% (Q4 of FY 2022-23), the minimum investment tenure is fixed at 15 years while the investment amount can range between Rs. 500 to Rs. 1.50 lakh in a financial year.")
+
+    with col2:
+        card_container2 = st.container()
+        with card_container2:
+            st.header("National Pension System (NPS)")
+            st.subheader("2 lakh")
+            st.write("NPS account tax benefits extend up to ₹2,00,000 per annum for each individual. As an investor, investing this amount will make you eligible to claim ₹1,50,000 tax deduction under Section 80C and an additional ₹50,000 under Section 80CCD(1B).")
+
+    with col3:
+        card_container3 = st.container()
+        with card_container3:
+            st.header("Sukanya Samriddhi")
+            st.subheader("1.5 lakh")
+            st.write("The minimum annual contribution to the Sukanya Samriddhi Account is Rs. 250 and the maximum contribution is Rs. 1.5 lakh in a financial year. You have to invest at least the minimum amount every year for up to 15 years from the date of account opening.")
+
+    col1, col2, col3 = st.columns(3, gap="large")
+    with col1:
+        card_container1 = st.container()
+        with card_container1:
+            st.header("Tax-Saving FD")
+            st.subheader("1.5 lakh")
+            st.write("A 5-year term deposit is also called a Tax-Saving FD. If you invest in one, you are eligible for tax deductions under Section 80C of the Income Tax Act, 1961. You can claim up to a maximum of Rs.1.5 lakh.")
+
+    with col2:
+        card_container2 = st.container()
+        with card_container2:
+            st.header("A Senior Citizens’ Saving Scheme (SCSS)")
+            st.subheader("1.08 lakh")
+            st.write("A Senior Citizens’ Saving Scheme (SCSS) is a government-backed retirement benefits programme. Senior citizens resident in India can invest a lump sum in the scheme, Get an income tax deduction of up to Rs.1.5 lakh under Section 80C of the Indian Tax Act, 1961.")
+
+    with col3:
+        card_container3 = st.container()
+        with card_container3:
+            st.header("ELSS")
+            st.subheader("1.08 lakh")
+            st.write("ELSS funds are equity funds that allow you to save tax while you invest for your long term goals. Investment in these funds can are eligible for deduction under Section 80c. These dual benefits mean anyone looking to invest up to Rs. 9,000 per month should only invest in this category.")
+
+
+
+
 def calculate(amount, percent):
 	return (amount * percent) / 100
 
@@ -264,3 +323,6 @@ def calculate_income_tax(total_income:float) -> float:
 
 def cagr(start_value, end_value, num_periods):
     return (end_value / start_value)**(1 / num_periods) - 1
+
+
+
