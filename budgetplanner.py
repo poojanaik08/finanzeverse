bcaba957f3c11f75a86604d91371717d844708ad
Pooja Naik <70855999+poojanaik08@users.noreply.github.com>
2023-03-12 09:08:13 +0530

    edited total income and some other things

--- budgetplanner.py
+++ budgetplanner.py
@@ -5,17 +5,12 @@ import streamlit.components.v1 as components
 import numpy as np
 
 def budgetplanner():
-    total_income = st.session_state.income_df["Value"].sum()
-    total_expense = st.session_state.expense_df["Value"].sum()
-    #total_savings = total_income - total_expense
-    total_savings = st.session_state.savings_df["Value"].sum()
-    # expense_per = total_expense/total_income
-    # saving_per = total_savings/total_income
-
+    income_df_copy = st.session_state.df.loc[st.session_state.df["Type"]=="Income"]
+    income_df_copy['Date'] = pd.to_datetime(income_df_copy['Date'])
+    income_df_2022 = income_df_copy[(income_df_copy['Date'] >= '2022-01-04')]
+    total_income = income_df_2022["Value"].sum()
     st.title("Your Tax Status:")
     total_income = int(total_income)
-    total_expense = int(total_expense)
-    total_savings = int(total_savings)
     tax = calculate_income_tax(total_income)					
     tax = int(tax)
 
@@ -249,7 +244,7 @@ def budgetplanner():
         st.plotly_chart(fig1)
 
     st.markdown("---")  
-    st.title("Here are some invewstement suggestion to save your Taxes:")
+    st.title("Here are some investement suggestions to save your Taxes:")
     st.markdown("###")
     with st.expander(label="Disclaimer"):
         st.write("Some of the following schemes fall under the Section 80C; according to which you can only get benefits upto 1.5 Lakh collectively.")
@@ -258,43 +253,50 @@ def budgetplanner():
     with col1:
         card_container1 = st.container()
         with card_container1:
-            st.header("PPF")
+            #st.header("PPF")
+            st.markdown("""<h2 style="color:#DA70D6;">PPF</h2>""",unsafe_allow_html=True)
             st.subheader("1.5 lakh")
             st.write("The current PPF interest rate is 7.1% (Q4 of FY 2022-23), the minimum investment tenure is fixed at 15 years while the investment amount can range between Rs. 500 to Rs. 1.50 lakh in a financial year.")
 
     with col2:
         card_container2 = st.container()
         with card_container2:
-            st.header("National Pension System (NPS)")
+            #st.header("National Pension System (NPS)")
+            st.markdown("""<h2 style="color:#DA70D6;">National Pension System (NPS)</h2>""",unsafe_allow_html=True)
             st.subheader("2 lakh")
             st.write("NPS account tax benefits extend up to ₹2,00,000 per annum for each individual. As an investor, investing this amount will make you eligible to claim ₹1,50,000 tax deduction under Section 80C and an additional ₹50,000 under Section 80CCD(1B).")
 
     with col3:
         card_container3 = st.container()
         with card_container3:
-            st.header("Sukanya Samriddhi")
+            #st.header("Sukanya Samriddhi")
+            st.markdown("""<h2 style="color:#DA70D6;">Sukanya Samriddhi</h2>""",unsafe_allow_html=True)
             st.subheader("1.5 lakh")
             st.write("The minimum annual contribution to the Sukanya Samriddhi Account is Rs. 250 and the maximum contribution is Rs. 1.5 lakh in a financial year. You have to invest at least the minimum amount every year for up to 15 years from the date of account opening.")
 
+    st.markdown("###")
     col1, col2, col3 = st.columns(3, gap="large")
     with col1:
         card_container1 = st.container()
         with card_container1:
-            st.header("Tax-Saving FD")
+            #st.header("Tax-Saving FD")
+            st.markdown("""<h2 style="color:#DA70D6;">Tax-Saving FD</h2>""",unsafe_allow_html=True)
             st.subheader("1.5 lakh")
             st.write("A 5-year term deposit is also called a Tax-Saving FD. If you invest in one, you are eligible for tax deductions under Section 80C of the Income Tax Act, 1961. You can claim up to a maximum of Rs.1.5 lakh.")
 
     with col2:
         card_container2 = st.container()
         with card_container2:
-            st.header("A Senior Citizens’ Saving Scheme (SCSS)")
+            #st.header("A Senior Citizens’ Saving Scheme (SCSS)")
+            st.markdown("""<h2 style="color:#DA70D6;">A Senior Citizens’ Saving Scheme (SCSS)</h2>""",unsafe_allow_html=True)
             st.subheader("1.08 lakh")
             st.write("A Senior Citizens’ Saving Scheme (SCSS) is a government-backed retirement benefits programme. Senior citizens resident in India can invest a lump sum in the scheme, Get an income tax deduction of up to Rs.1.5 lakh under Section 80C of the Indian Tax Act, 1961.")
 
     with col3:
         card_container3 = st.container()
         with card_container3:
-            st.header("ELSS")
+            #st.header("ELSS")
+            st.markdown("""<h2 style="color:#DA70D6;">ELSS</h2>""",unsafe_allow_html=True)
             st.subheader("1.08 lakh")
             st.write("ELSS funds are equity funds that allow you to save tax while you invest for your long term goals. Investment in these funds can are eligible for deduction under Section 80c. These dual benefits mean anyone looking to invest up to Rs. 9,000 per month should only invest in this category.")
 
